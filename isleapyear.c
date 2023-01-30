06a15d27fbfe23ec0423e48860ed9204a48338ec
1157350959 <102706044+1157350959@users.noreply.github.com>
2023-01-30 10:54:35 -0500

    Update isleapyear.c

--- isleapyear.c
+++ isleapyear.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
-
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-    if((int)argv % 4 == 0)
+    if(atoi(argv[1]) % 4 == 0)
     {
-        if((int)argv % 100 == 0 && (int)argv % 400 != 0)
+        if(atoi(argv[1]) % 100 == 0 && atoi(argv[1]) % 400 != 0)
         {
-            printf("%s was not a leap year\n", argv);
+            printf("%s was not a leap year\n", argv[1]);
         }
         else
         {
-            printf("%s was a leap year\n", argv);
+            printf("%s was a leap year\n", argv[1]);
         }
     }
     else
     {
-        printf("%s was not a leap year\n", argv);
+        printf("%s was not a leap year\n", argv[1]);
     }
     return 0;
 }
